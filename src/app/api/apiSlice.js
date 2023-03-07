cae05150be2a9a67fba475d43bf30a5cbfc2535c
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-07 13:58:07 +0100

    cleaning apislice

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { setCredentials, logOut } from '../../features/auth/AuthSlice'
+// import { setCredentials, logOut } from '../../features/auth/AuthSlice'
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'http://localhost:3500',
@@ -13,27 +13,28 @@ const baseQuery = fetchBaseQuery({
   },
 })
 
-const baseQueryWithReauth = async (args, api, extraOptions) => {
-  let result = await baseQuery(args, api, extraOptions)
-  if (result?.error?.originalStatus === 403) {
-    console.log('sending refresh token')
-    // send refresh token to get new access token
-    const refreshResult = await baseQuery('/refresh', api, extraOptions)
-    console.log(refreshResult)
-    if (refreshResult?.data) {
-      const user = api.getState().auth.user
-      // store the new token
-      api.dispatch(setCredentials({ ...refreshResult.data, user }))
-      // retry the original query with the new token
-      result = await baseQuery(args, api, extraOptions)
-    } else {
-      api.dispatch(logOut())
-    }
-  }
-  return result
-}
-
 export const apiSlice = createApi({
-    baseQuery: baseQueryWithReauth,
-    endpoints: builder => ({})
+  baseQuery: baseQuery,
+  endpoints: builder => ({})
 })
+
+// const baseQueryWithReauth = async (args, api, extraOptions) => {
+//   let result = await baseQuery(args, api, extraOptions)
+//   if (result?.error?.originalStatus === 403) {
+//     console.log('sending refresh token')
+//     // send refresh token to get new access token
+//     const refreshResult = await baseQuery('/refresh', api, extraOptions)
+//     console.log(refreshResult)
+//     if (refreshResult?.data) {
+//       const user = api.getState().auth.user
+//       // store the new token
+//       api.dispatch(setCredentials({ ...refreshResult.data, user }))
+//       // retry the original query with the new token
+//       result = await baseQuery(args, api, extraOptions)
+//     } else {
+//       api.dispatch(logOut())
+//     }
+//   }
+//   return result
+// }
+
