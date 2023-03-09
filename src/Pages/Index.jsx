5807d3ee79893c3cd319007e7abf22ce24ef7354
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-09 11:12:13 +0100

    Init Main Layout + some css

---
+++ src/Pages/Index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Nav from '../components/Nav/Nav'
+import Footer from '../components/Footer/Footer'
+
+
+
+
+const Index = () => {
+
+  return (
+    <>
+    
+    <div>Index</div>
+    
+    </>
+   
+  )
+}
+
+export default Index
--- src/routes.jsx
+++ src/routes.jsx
@@ -6,6 +6,9 @@ import Layout from './components/Layout'
 import Login from './features/auth/Auth'
 import Welcome from './features/auth/Welcome'
 import RequireAuth from './features/auth/RequireAuth'
+import Index from './Pages/Index'
+import Nav from './components/Nav/Nav'
+import Footer from './components/Footer/Footer'
 
 /**
  * If the user is not logged in, show the login page, otherwise show the dashboard.
@@ -13,20 +16,25 @@ import RequireAuth from './features/auth/RequireAuth'
 
 export function Routing(props) {
   return (
-    <Routes>
-
-      <Route path="/" element={<Layout />}>
-        {/* public Routes */}
-        <Route index element={<Public />} />
-        <Route path="login" element={<Login />} />
-        {/* private Routes */}
-        <Route element={<RequireAuth />}>
-          <Route path="main" element={<Welcome />} />
-          {/* <Route path="main" element={<Main />} /> */}
+    <>
+      <Nav />
+      <main className="main bg-dark">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public Routes */}
+          <Route index element={<Login />} />
+          {/* <Route path="login" element={<Public />} /> */}
+          {/* private Routes */}
+          <Route element={<RequireAuth />}>
+            {/* <Route path="main" element={<Welcome />} /> */}
+            <Route path="main" element={<Index />} />
+            {/* <Route path="main" element={<Main />} /> */}
+          </Route>
         </Route>
-
-      </Route>
-    </Routes>
+      </Routes>
+      </main>
+      <Footer />
+    </>
   )
 }
 
