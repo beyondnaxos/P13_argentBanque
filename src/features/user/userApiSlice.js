d4f6442ffcae23a37c3b9e804af68aa42452b631
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-12 08:20:48 +0100

    Importing Token

--- src/features/user/userApiSlice.js
+++ src/features/user/userApiSlice.js
@@ -1,16 +1,19 @@
-import { apiSlice } from "../../app/api/apiSlice"
+import { apiSlice } from '../../app/api/apiSlice'
+import { useSelector } from 'react-redux'
+import { selectCurrentToken } from '../auth/AuthSlice'
 
+const token = useSelector(selectCurrentToken)
 export const userApiSlice = apiSlice.injectEndpoints({
-    endpoints: builder => ({
-        login : builder.mutation({
-            query: credentials => ({
-                url: '/user/profile', 
-                method: 'PUT',
-                body: {...credentials}
-            })
-        }),
-        
-    })
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: '/user/profile',
+        method: 'PUT',
+        headers: { Authorization: `Bearer ${token}` },
+        body: { ...credentials },
+      }),
+    }),
+  }),
 })
 
 export const { useLoginMutation } = userApiSlice
