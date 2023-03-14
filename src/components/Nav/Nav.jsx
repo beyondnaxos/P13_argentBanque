b887f6269e33e4cf6267823415316f4686aa0ce0
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-14 14:07:45 +0100

    Indent

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { logOut } from '../../features/auth/AuthSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { selectCurrentFirstname, selectCurrentLastname } from '../../features/user/userSlice'
+import {
+  selectCurrentFirstname,
+  selectCurrentLastname,
+} from '../../features/user/userSlice'
 // import { useSelector } from 'react-redux'
 // import { selectCurrentToken } from '../../features/auth/AuthSlice'
 
 const Nav = () => {
-
   const userFirstname = useSelector(selectCurrentFirstname)
   const userLastname = useSelector(selectCurrentLastname)
 
@@ -29,20 +31,26 @@ const Nav = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </a>
       <div>
-        <p>{userFirstname} {userLastname}</p>
+        <p>
+          {userFirstname} {userLastname}
+        </p>
         {token ? (
-        <span role='button' className="main-nav-item" onClick={() => {
-          dispatch(logOut())
-          navigate('/')
-        }}>
-          <i className="fa fa-sign-out"></i>
-          Sign Out
-        </span>
+          <span
+            role="button"
+            className="main-nav-item"
+            onClick={() => {
+              dispatch(logOut())
+              navigate('/')
+            }}
+          >
+            <i className="fa fa-sign-out"></i>
+            Sign Out
+          </span>
         ) : (
-        <a className="main-nav-item" href="/login">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </a>
+          <a className="main-nav-item" href="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </a>
         )}
       </div>
     </nav>
