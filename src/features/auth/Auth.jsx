7862125dc575fccae1ec93efd4fa46c6aeb903fd
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-18 13:25:23 +0100

    errormsg  + rememberMe

--- src/features/auth/Auth.jsx
+++ src/features/auth/Auth.jsx
@@ -11,10 +11,20 @@ import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 function Auth() {
   const userRef = useRef()
   const errRef = useRef()
+  const [isChecked, setIsChecked] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [errMsg, setErrMsg] = useState('')
   const navigate = useNavigate()
+
+  const handleChange = (event) => {
+    if (event.target.checked) {
+      console.log('✅ Checkbox is checked')
+    } else {
+      console.log('⛔️ Checkbox is NOT checked')
+    }
+    setIsChecked((current) => !current)
+  }
 
   const [login, { isLoading }] = useLoginMutation()
   const dispatch = useDispatch()
@@ -36,8 +46,9 @@ function Auth() {
       setEmail('')
       setPassword('')
       navigate('/profile')
-      localStorage.setItem('token', userData.body.token);
-
+      if (isChecked) {
+        localStorage.setItem('token', userData.body.token)
+      }
     } catch (err) {
       if (!err?.response) {
         setErrMsg('Server is not responding')
@@ -57,50 +68,60 @@ function Auth() {
   const handleEmailInput = (e) => setEmail(e.target.value)
   const handlePasswordInput = (e) => setPassword(e.target.value)
 
+
+
   const content = isLoading ? (
     <h1>Loading...</h1>
   ) : (
     <main className="main bg-dark">
-    <section className="sign-in-content">
-      <FontAwesomeIcon icon={faUserCircle} className="sign-in-icon" />
-      {/* <i class="fa fa-user-circle sign-in-icon"></i> */}
-      <h1 className={styles.formTitle}>Sign In</h1>
+      <section className="sign-in-content">
+        <FontAwesomeIcon icon={faUserCircle} className="sign-in-icon" />
+        {/* <i class="fa fa-user-circle sign-in-icon"></i> */}
+        <h1 className={styles.formTitle}>Sign In</h1>
 
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className="input-wrapper">
-          <label htmlFor="email">Username</label>
-          <input
-            className={styles.formInput}
-            color="black"
-            type="text"
-            id="email"
-            ref={userRef}
-            value={email}
-            onChange={handleEmailInput}
-            autoComplete="off"
-            required
-          />
-        </div>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="email">Username</label>
+            <input
+              className={styles.formInput}
+              color="black"
+              type="text"
+              id="email"
+              ref={userRef}
+              value={email}
+              onChange={handleEmailInput}
+              autoComplete="off"
+              required
+            />
+          </div>
 
-        <div className="input-wrapper">
-          <label htmlFor="password">Password</label>
-          <input
-            className={styles.formInput}
-            type="password"
-            id="password"
-            onChange={handlePasswordInput}
-            value={password}
-            required
-          />
-        </div>
-        <div className="input-remember">
-          <input type="checkbox" id="remember-me" />
-          <label htmlFor="remember-me">Remember me</label>
-        </div>
-        {/* <button className={styles.formBtn}>Sign In</button> */}
-        <button className="sign-in-button">Sign In</button>
-      </form>
-    </section>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              className={styles.formInput}
+              type="password"
+              id="password"
+              onChange={handlePasswordInput}
+              value={password}
+              required
+            />
+          </div>
+          <div className="input-remember">
+            <input
+              type="checkbox"
+              id="remember-me"
+              value={isChecked}
+              onChange={handleChange}
+            />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <div className="input-error" ref={errRef} tabIndex="-1">
+            {errMsg}
+          </div>
+          {/* <button className={styles.formBtn}>Sign In</button> */}
+          <button className="sign-in-button">Sign In</button>
+        </form>
+      </section>
     </main>
   )
 
